de389b947d788dc9bb33a5e3c1683f0edb49922c
Geewon Lee <gwlee1991@gmail.com>
2017-11-16 21:18:46 -0800

    finish string_compression

---
+++ arraysAndStrings/1.6.rb
@@ -0,0 +1,45 @@
+# String Compression: Implement a method to perform 
+# basic string compression using the counts of repeated 
+# characters. For example, the string aabcccccaaa 
+# would become a2blc5a3. If the "compressed" string 
+# would not become smaller than the original string, 
+# your method should return the original string. You 
+# can assume the string has only uppercase and lowercase 
+# letters (a - z).
+
+# just need to iterate through the string once so it takes
+# O(n) time and O(1) space.
+
+
+def string_compression (string) 
+  compressed = ""
+
+  count = 1
+  for i in 0..(string.length - 1)
+    if i == 0
+      compressed += string[i]
+      next
+    end
+
+    if compressed[-1] == string[i]
+      count += 1
+    else
+      compressed += count.to_s
+      count = 1
+      compressed += string[i]
+    end
+
+    if i == string.length - 1
+      compressed += count.to_s
+    end
+  end
+
+  if compressed.length > string.length
+    string
+  else
+    compressed
+  end
+end
+
+
+p string_compression('aabcccccaaa')
