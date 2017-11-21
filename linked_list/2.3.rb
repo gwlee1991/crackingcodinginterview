891b142721de94a446ff341362656f40d10da700
Geewon Lee <gwlee1991@gmail.com>
2017-11-20 21:12:59 -0800

    delete middle node solution

---
+++ linked_list/2.3.rb
@@ -0,0 +1,12 @@
+# Delete middle node: Implement an algorithm to delete a node
+# in the middle of a singly linked list, given only access to
+# that node.
+
+# simple solution that takes O(1) since all we have to do is
+# reassign the pointers
+
+def delete_middle_node (target)
+  target.prev.next = target.next
+  target.next.prev = target.prev
+  true
+end
