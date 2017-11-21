acc82ee3655e625a8cad4ca1ff2be455bb98d9f4
Geewon Lee <gwlee1991@gmail.com>
2017-11-20 20:47:40 -0800

    kth to last in linked list

---
+++ linked_list/2.1.rb
@@ -0,0 +1,34 @@
+require_relative 'linked_list'
+# Remove Dups: Write code to remove duplicates from
+# an unsorted linked list.
+# How would you solve this problem if a temporary buffer
+# is not allowed?
+
+
+# assumption => list is a doubly linked list
+# list has a val, next, prev
+# duplicate indicates nodes with same values
+# even if node object id is different
+def remove_dups (list) 
+  vals = {}
+  list.each do |node|
+    if vals.values.include?(node.val)
+      node.remove
+    else
+      vals[node] = node.val
+    end
+  end
+  list
+end
+
+linkedlist = LinkedList.new
+node1 = Link.new(5)
+node2 = Link.new(4)
+node3 = Link.new(3)
+linkedlist.append(node1)
+linkedlist.append(node2)
+linkedlist.append(node3)
+linkedlist.append(node1)
+linkedlist.append(node1)
+
+p remove_dups(linkedlist)
---
+++ linked_list/linked_list.rb
@@ -0,0 +1,82 @@
+class Link
+  attr_accessor :key, :val, :next, :prev
+
+  def initialize(val = nil)
+    @val = val
+    @next = nil
+    @prev = nil
+  end
+
+  def to_s
+    "#{@val}"
+  end
+
+  def remove
+    self.prev.next = self.next if self.prev != nil
+    self.next.prev = self.prev if self.next != nil
+    self.next = nil
+    self.prev = nil
+
+    # optional but useful, connects previous link to next link
+    # and removes self from list.
+  end
+end
+
+class LinkedList
+  include Enumerable
+  attr_reader :list
+  def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
+    # @link = link.new
+  end
+
+  def [](i)
+    each_with_index { |link, j| return link if i == j }
+    nil
+  end
+
+  def first
+    @head.next
+  end
+
+  def last
+    @tail.prev
+  end
+
+  def empty?
+    @tail.prev == @head
+  end
+
+  def append(link)
+    @tail.prev.next = link # setting before reassigning tails values
+    link.prev = @tail.prev
+    link.next = @tail
+    @tail.prev = link
+  end
+
+
+  def remove(val)
+    each do |link|
+      if link.val == val
+        link.remove
+        break
+      end
+    end
+  end
+
+  def each(&prc)
+    first = @head.next
+    while first != @tail
+      prc.call(first)
+      first = first.next
+    end
+  end
+
+  # uncomment when you have `each` working and `Enumerable` included
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
+end
