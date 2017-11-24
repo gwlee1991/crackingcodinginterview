7d50ff67d487e5b17c4e3bac0f7e463f5c539280
Geewon Lee <gwlee1991@gmail.com>
2017-11-23 19:38:25 -0800

    loop detection solution

--- linked_list/2.8.rb
+++ linked_list/2.8.rb
@@ -2,3 +2,24 @@ require_relative 'linked_list'
 
 # Loop Detection: Given a circular linked list, implement an
 # algorithm that returns the node at the beginning of the loop.
+
+
+# create a hash to store nodes and iterate through the list.
+# once we hit a node that is already in the hash,
+# we know that, that is the starting point of the circle loop.
+
+def loop_detection(list)
+  nodes = Hash.new
+
+  head = list.head.next #first element in the list
+  until head == nil
+    if nodes[head]
+      return head
+    else
+      nodes[head] = true
+    end
+    head = head.next
+  end
+
+  nil
+end
