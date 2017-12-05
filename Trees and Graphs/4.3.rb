ced1cc81b33bbefa0df7ef916e7b914a698adbc9
Geewon Lee <gwlee1991@gmail.com>
2017-12-04 22:39:14 -0800

    list of depths solution

--- Trees and Graphs/4.3.rb
+++ Trees and Graphs/4.3.rb
@@ -2,6 +2,17 @@
 # which creates a linked list of all the nodes at each depth
 # (e.g., if you have a tree with depth D, you'll have D linked list)
 
-def create_linked_list (root, level, )
+def create_level_linked_list (root, lists, level)
+  return if root == nil
 
+  list = nil
+  if lists.length == level
+    list = LinkedList.new
+    lists.add(list)
+  else
+    list = lists[level]
+  end
+end
+
+class LinkedList
 end
