d83adb6d03406e89579e8a12c4bc68365bfb99d9
Geewon Lee <gwlee1991@gmail.com>
2017-12-04 21:24:42 -0800

    write down questions for list of depths and check balanced

--- Trees and Graphs/4.3.rb
+++ Trees and Graphs/4.3.rb
@@ -2,3 +2,6 @@
 # which creates a linked list of all the nodes at each depth
 # (e.g., if you have a tree with depth D, you'll have D linked list)
 
+def create_linked_list (root, level, )
+
+end
---
+++ Trees and Graphs/4.4.rb
@@ -0,0 +1,8 @@
+# Check Balanced: Implement a function to check if a binary tree
+# is balanced. For the purposes of this question, a balance tree is 
+# defined to be a tree such that the heights of the two subtress of 
+# any node never differ by more than once.
+
+# we need to do this recursively.
+# our check would be when either right or left's node is nil
+# the node that is not nil should not have a child node.
