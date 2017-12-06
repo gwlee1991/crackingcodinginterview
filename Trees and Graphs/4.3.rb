ecef73ea73c9840da8d0bce0c4eeee44a8db5a7d
Geewon Lee <gwlee1991@gmail.com>
2017-12-05 21:43:20 -0800

    add a breadth-first search implementation for list of depths

--- Trees and Graphs/4.3.rb
+++ Trees and Graphs/4.3.rb
@@ -2,6 +2,9 @@
 # which creates a linked list of all the nodes at each depth
 # (e.g., if you have a tree with depth D, you'll have D linked list)
 
+
+# depth first search implementation
+
 def create_level_linked_list (root, lists, level)
   return if root == nil
 
@@ -17,6 +20,31 @@ def create_level_linked_list (root, lists, level)
   create_level_linked_list(root.righ, lists, level + 1)
 end
 
+# breadth-first search implementation
+
+def create_level_linked_list2(root)
+  result = []
+  current = LinkedList.new
+  if root != nil
+    current.append(root)
+  end
+
+  while current.length > 0
+    result.push(current)
+    parents = current
+    current = LinkedList.new
+    parents.each do
+      if parent.left != nil
+        current.append(parent.left)
+      end
+
+      if parent.right != nil
+        current.append(parent.right)
+      end
+    end
+  end
+  result
+end
 
 class LinkedList
   def initalize
