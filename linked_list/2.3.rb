8694ddf7194ffcf4221d72982757b691a4f30d5a
Geewon Lee <gwlee1991@gmail.com>
2017-12-07 11:29:47 -0800

    change 2.3.rb to use a singly linked list

--- linked_list/2.3.rb
+++ linked_list/2.3.rb
@@ -6,8 +6,19 @@ require_relative 'linked_list'
 # simple solution that takes O(1) since all we have to do is
 # reassign the pointers
 
+
+# for doubly
 def delete_middle_node (target)
   target.prev.next = target.next
   target.next.prev = target.prev
+  true
+end
+
+# for singly
+# copy info from next node and delete the next node.
+
+def delete_middle_node2(target)
+  target.data = target.next.data
+  target.next = target.next.next
   true
 end
