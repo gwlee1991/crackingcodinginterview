8549443c0781b3f70e70827085e2ea840282176d
Younis Hamalawy <younis.hamalawy@gmail.com>
2018-01-05 14:15:36 -0800

    Solved 1.7 rotate_matrix

--- arraysAndStrings/1.7.rb
+++ arraysAndStrings/1.7.rb
@@ -1,23 +1,36 @@
-# 1.7 Rotate Matrix: Given an image represented by an 
-# NxN matrix, where each pixel in the image is 4 bytes, 
-# write a method to rotate the image by 90 degrees. 
+# 1.7 Rotate Matrix: Given an image represented by an
+# NxN matrix, where each pixel in the image is 4 bytes,
+# write a method to rotate the image by 90 degrees.
 # Can you do this in place?
 
-# [
-# [[0,0], [0,1], [0,2], [0,3]],
-# [[1,0], [1,1], [1,2], [1,3]],
-# [[2,0], [2,1], [2,2], [2,3]],
-# [[3,0], [3,1], [3,2], [3,3]]
-# ]
 
-# [0,0] => [0,3]
-# [0,3] => [3,3]
-# [3,3] => [0,3]
-# [3,0] => [0,0]
+# time complicity O(n^2)
+def rotate_matrix(matrix)
+  arr = []
+  matrix.length.times do |i|
+    sub = []
 
-# [
-#   [[3,0], [2,0], [1,0], [0,0]],
-#   [[3,1], [2,1], [1,1], [0,1]],
-#   [[3,2], [2,2], [1,2], [0,2]],
-#   [[3.3], [2,3], [1,3], [0,3]]
-# ]
+    matrix.each do |array|
+      sub << array[i]
+    end
+
+    arr << sub.reverse
+  end
+  arr
+end
+
+my_matrix = [
+[[0,0], [0,1], [0,2], [0,3]],
+[[1,0], [1,1], [1,2], [1,3]],
+[[2,0], [2,1], [2,2], [2,3]],
+[[3,0], [3,1], [3,2], [3,3]]
+]
+
+
+p rotate_matrix(my_matrix) == [
+  [[3,0], [2,0], [1,0], [0,0]],
+  [[3,1], [2,1], [1,1], [0,1]],
+  [[3,2], [2,2], [1,2], [0,2]],
+  [[3,3], [2,3], [1,3], [0,3]]
+]
+
