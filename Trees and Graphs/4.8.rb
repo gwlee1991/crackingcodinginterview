9491ffa1958d6f77fe0de6212e801708ea6b27e5
Geewon Lee <gwlee1991@gmail.com>
2018-03-05 15:53:31 -0800

    add solution to 4.8 common ancestor

--- Trees and Graphs/4.8.rb
+++ Trees and Graphs/4.8.rb
@@ -3,15 +3,45 @@
 # additional nodes in a data structure. NOTE: This is not necessarily a
 # binary search tree.
 
-# If we traverse through the tree and realize that the nodes are on opposite
-# sides of the tree, we know that the current node is the common ancestor for 
-# nodes p and q. For example, if p is somewhere on the right side of the root node
-# and q is somewhere on the left side of the root node, root node is the first
-# common ancestor. If they are on the same side(lets say left), then we change our
-# pointer to the left child of the root node and traverse through the rest of the tree.
-# We call this recursively.
+# assuming that each node has access to it's parent's node, we traverse up
+# the tree. Because the nodes can be in different depths of the tree, we
+# initially calculate the depths by traversing the tree from the root node.
+# Once we know the depth of each node, we make sure that we move up the node
+# that is deeper with in the tree to the save level as the other node.
+# After that we check the parent of each node. Once, the parent is the same,
+# we return that node.
 
-# assumptions
-# p and q are present on the tree
-# all nodes have a parent, left, right field.
+def common_ancestor(p , q)
+  diff = depth(p) - depth(q)
+  deep = diff > 0 ? p : q
+  shallow = diff > 0 ? q : p
+  deep = moveUp(deep, diff.abs)
 
+  while deep != shallow && shallow != nil && deep != nil
+    shallow = shallow.parent
+    deep = deep.parent
+  end
+
+  if shallow == nil || deep == nil
+    return nil
+  else
+    return shallow
+  end
+end
+
+def moveUp(node, diff)
+  while (diff > 0 && node != nil)
+    node = node.parent
+    diff -= 1
+  end
+  node
+end
+
+def depth(node)
+  level = 0
+  while node != nil
+    node = node.parent
+    level += 1
+  end
+  level
+end
