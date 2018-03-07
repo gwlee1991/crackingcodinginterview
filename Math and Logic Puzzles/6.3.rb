8ac8280ce0aa55a07c4d1385844317f993211083
Geewon Lee <gwlee1991@gmail.com>
2018-03-07 15:40:29 -0800

    dominos question and solution

---
+++ Math and Logic Puzzles/6.3.rb
@@ -0,0 +1,10 @@
+# Dominos: There is an 8x8 chessboard in which two diagonaly opposite corners have been
+# cut off. You are given 31 dominos, and a single domino can cover exactly two squares.
+# Can you use the 31 dominos to cover the entire board? Prove your answer(by providing an
+# example or showing why it's impossible).
+
+# solution
+# Diagonally opposite corners on a chessboard have same colors. This means that the chessboard
+# will have either 30 white 32 black squares or 32white 30 black squares. However, a domino
+# has to cover 1 white and 1 black square. For the 31 dominos to cover the chessboard, the 
+# chessboard requires to have 31 white and 31 black squares. This is why it is impossible.
