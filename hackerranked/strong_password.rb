1e7530fec02e0ba3bca534ff531b2fee13f3a53f
Geewon Lee <gwlee1991@gmail.com>
2018-03-07 15:51:50 -0800

    hackerranked question added - strong password

---
+++ hackerranked/strong_password.rb
@@ -0,0 +1,13 @@
+# Its length is at least 6.
+# It contains at least one digit.
+# It contains at least one lowercase English character.
+# It contains at least one uppercase English character.
+# It contains at least one special character. The special characters are: !@#$%^&*()-+
+
+# numbers = "0123456789"
+# lower_case = "abcdefghijklmnopqrstuvwxyz"
+# upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# special_characters = "!@#$%^&*()-+"
+
+# input will be n, the length of the string, and pw, the typed in password.
+# should return how many digits must be added to be considered a strong password.
