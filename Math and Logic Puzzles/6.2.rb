9dad8f82dde0254697a4dd3060b99edc05e358f8
Geewon Lee <gwlee1991@gmail.com>
2018-03-06 21:26:39 -0800

    6.2 question

---
+++ Math and Logic Puzzles/6.2.rb
@@ -0,0 +1,6 @@
+# Basketball: You have a basketball hoop and someone says that you can
+# play one of two games.
+# game1: You get one shot to make the hoop.
+# game2: You get three shots and you have to make two of three shots.
+# if p is the probability of making a particular shot, for which values of
+# p should you pick one game or the other?
