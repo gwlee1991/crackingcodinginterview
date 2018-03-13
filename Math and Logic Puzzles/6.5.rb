008ee66dbd5dc940ee9caf77a2820d3cd03954cd
Geewon Lee <gwlee1991@gmail.com>
2018-03-12 17:32:56 -0700

    solution to jugs of water from CCI

---
+++ Math and Logic Puzzles/6.5.rb
@@ -0,0 +1,11 @@
+# Jugs of Water: You have a 5-quart jug, a 3-quart jug, and an unlimited supply of water (but
+# no measuring cups). How would you come up with exactly 4- quarts of water? Note that the jugs
+# are oddly shaped. such that filling up exactly "half" of the jug would be impossible.
+
+# ez
+# fill 3-j and pour it into 5-j.
+# fill 3-j and pour it into 5-j. This leaves 1 quart in the 3-j.
+# empty 5-j and pour the 1 quart from 3-j to 5-j.
+# fill 3-j.
+# pour 3quarts in 3-j to 5-j.
+# 4 quarts.
