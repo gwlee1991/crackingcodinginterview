535a3dc2fedd574eb60d7f4afa37d33e7dded705
Geewon Lee <gwlee1991@gmail.com>
2018-03-28 23:11:44 -0700

    partially work on a solution for deck of cards

--- Object-Oriented-Design/7.1.rb
+++ Object-Oriented-Design/7.1.rb
@@ -1,3 +1,26 @@
 # Deck of Crrds: Design the data structures for a generic deck of cards. Explain how you would
 # subclass the data structures to implement blackjack.
 
+# individual cards
+class Card
+  attr_reader :suit, :value
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
+end
+
+class Deck
+  :SUITS = ["heart", "spade", "diamond", "club"]
+  :VALUES = ["1","2","3","4","5","6","7","8","9","10","J", "Q", "K"]
+
+  def self.populate(decks)
+    deck = []
+    :SUITS.map {}
+  end
+
+  def initialize(decks)
+    @decks = Deck.populate(decks)
+  end
+end
+
